1d98223f888b491a1b3d5773ace6d89527e4a1a5
SamuelRicardos <samuelric4rdo@gmail.com>
2025-05-05 21:54:29 -0300

    organizacao do projeto - fim da aula 6

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,12 @@
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
+
+import { UserContextProvider } from "./contexts/UserContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Account from "./pages/Account";
-import { UserContextProvider } from "./contexts/UserContext";
+import Header from "./components/Header";
 
 axios.defaults.baseURL = import.meta.env.VITE_AXIOS_BASE_URL;
 axios.defaults.withCredentials = true;
